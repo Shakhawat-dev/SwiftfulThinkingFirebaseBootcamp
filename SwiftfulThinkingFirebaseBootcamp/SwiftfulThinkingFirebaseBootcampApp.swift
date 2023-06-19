f6bea8f8ded1eaac81ee34b8135d583c24d8ca5c
Md Shakhawat Hossain Shahin <shakhawat.dev@gmail.com>
2023-06-19 17:38:43 +0600

    Initial Commit

---
+++ SwiftfulThinkingFirebaseBootcamp/SwiftfulThinkingFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingFirebaseBootcampApp.swift
+//  SwiftfulThinkingFirebaseBootcamp
+//
+//  Created by Shakhawat Hossain Shahin on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
